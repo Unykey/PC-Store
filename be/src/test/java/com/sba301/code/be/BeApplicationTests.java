a5444ea75da23ba772c3610c8a66ab86451aede8
Huỳnh Hoàng Long <long230205@gmail.com>
2026-01-17 08:50:03 +0700

    Add backend module

---
+++ be/src/test/java/com/sba301/code/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba301.code.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
