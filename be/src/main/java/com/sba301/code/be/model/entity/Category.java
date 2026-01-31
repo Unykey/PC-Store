766d825c5519ecdeadafcf5b7ce391751c383bf9
backenddev01-dev <59830068+Danciel@users.noreply.github.com>
2026-01-31 07:49:59 +0700

    Edit lombok second time

--- be/src/main/java/com/sba301/code/be/model/entity/Category.java
+++ be/src/main/java/com/sba301/code/be/model/entity/Category.java
@@ -7,9 +7,10 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Table(name = "category_tbl")
+@Table(name = "category")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Category {
 
     @Id
